114e843701403d06e828cf15ccb8b071f4a04441
Jeznar <95548360+Jeznar@users.noreply.github.com>
2022-03-19 08:10:40 -0400

    Taught flameseed to look at token and item iamge for color string.

--- Class-Features/Druid/Wildfire_2_Summon_Widlfire_Spirit/Flame_Seed/Flame_Seed.js
+++ Class-Features/Druid/Wildfire_2_Summon_Widlfire_Spirit/Flame_Seed/Flame_Seed.js
@@ -1,48 +1,55 @@
 const MACRONAME = "Flame_Seed.js"
 /*****************************************************************************************
- * Basic Structure for a rather complete macro
+ * Run a two seto VFX that changes based on target hit or miss
  * 
  * 02/11/22 0.1 Creation of Macro
  *****************************************************************************************/
 const MACRO = MACRONAME.split(".")[0]     // Trim of the version number and extension
-jez.log(`============== Starting === ${MACRONAME} =================`);
-for (let i = 0; i < args.length; i++) jez.log(`  args[${i}]`, args[i]);
 const LAST_ARG = args[args.length - 1];
-let aActor;         // Acting actor, creature that invoked the macro
 let aToken;         // Acting token, token for creature that invoked the macro
+if (LAST_ARG.tokenId) aToken = canvas.tokens.get(LAST_ARG.tokenId); 
+    else aToken = game.actors.get(LAST_ARG.tokenId);
 let aItem;          // Active Item information, item invoking this macro
-if (LAST_ARG.tokenId) aActor = canvas.tokens.get(LAST_ARG.tokenId).actor; else aActor = game.actors.get(LAST_ARG.actorId);
-if (LAST_ARG.tokenId) aToken = canvas.tokens.get(LAST_ARG.tokenId); else aToken = game.actors.get(LAST_ARG.tokenId);
-if (args[0]?.item) aItem = args[0]?.item; else aItem = LAST_ARG.efData?.flags?.dae?.itemData;
-const CUSTOM = 0, MULTIPLY = 1, ADD = 2, DOWNGRADE = 3, UPGRADE = 4, OVERRIDE = 5;
-let msg = "";
-let tToken = canvas.tokens.get(args[0]?.targets[0]?.id); // First Targeted Token, if any
-jez.log("tToken",tToken)
+if (args[0]?.item) aItem = args[0]?.item; 
+    else aItem = LAST_ARG.efData?.flags?.dae?.itemData;
 
-
-// https://github.com/fantasycalendar/FoundryVTT-Sequencer/wiki/MidiQOL-&-JB2A-Fire-Bolt
-
-let tokenD = canvas.tokens.get(args[0].tokenId);
-let aItem;          // Active Item information, item invoking this macro
-if (args[0]?.item) aItem = args[0]?.item; else aItem = LAST_ARG.efData?.flags?.dae?.itemData;
-
-// COOL-THING: Pick VFX color based on color of item image icon
+// COOL-THING: Double VFX with color selected by item token
 let color = ""
+let expColor = ""
 const IMAGE = aItem.img.toLowerCase()
-if(IMAGE.includes("blue")) color = "blue"
-else if(IMAGE.includes("dark_green")) color = "dark_green"
-else if(IMAGE.includes("green02")) color = "green02"
-else if(IMAGE.includes("green")) color = "green"
-else if(IMAGE.includes("dark_red")) color = "dark_red"
-else if(IMAGE.includes("orange")) color = "orange"
-else if(IMAGE.includes("purple")) color = "purple"
-if (!color) color = "orange"
+if(IMAGE.includes("blue"))              {color ="blue";         expColor="blueyellow" }
+else if(IMAGE.includes("dark_green"))   {color="dark_green";    expColor="green" }
+else if(IMAGE.includes("green02"))      {color="green02";       expColor="green" }
+else if(IMAGE.includes("green"))        {color="green";         expColor="greenorange" }
+else if(IMAGE.includes("dark_red"))     {color="dark_red";      expColor="dark_red" }
+else if(IMAGE.includes("orange"))       {color="orange";        expColor="orange" }
+else if(IMAGE.includes("purple"))       {color="purple";        expColor="purple" }
+if (!color)                             {color="orange";        expColor="orange" }
 
-new Sequence()
-    .effect()
-        //.file("jb2a.fire_bolt.orange")
-        .file(`jb2a.fire_bolt.${color}`)
-        .atLocation(tokenD)
-        .reachTowards(args[0].targets[0])
-        .missed(args[0].hitTargets.length === 0)
-    .play()
+if (args[0].hitTargets[0]) { // Target was hit
+    new Sequence()
+        .effect()
+            .file(`jb2a.fire_bolt.${color}`)
+            .atLocation(aToken)
+            .stretchTo(args[0].targets[0])
+            //.missed(args[0].hitTargets.length === 0) // Keeping this for reference
+            .waitUntilFinished(-750) 
+        .effect()
+            .file(`jb2a.explosion.${expColor}.0`)
+            .scale(0.5)
+            .atLocation(args[0].hitTargets[0])
+        .play()
+} else {    // Target was missed
+    new Sequence()
+        .effect()
+            .file(`jb2a.fire_bolt.${color}`)
+            .atLocation(aToken)
+            .stretchTo(args[0].targets[0])
+            .missed(args[0].hitTargets.length === 0)
+        .play()
+}
+
+
+
+
+
