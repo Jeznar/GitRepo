49e58778dac45ffac266d6f41946c7b49667623d
Jeznar <95548360+Jeznar@users.noreply.github.com>
2022-06-29 15:51:30 -0400

    Addressed player permission issue for game.scenes.current.createEmbeddedDocuments(Tile... and canvas.scene.deleteEmbeddedDocuments(Tile... calls in **Ice_Storm**

--- Spells/2nd_Level/Gust_of_Wind/Gust_of_Wind.js
+++ Spells/2nd_Level/Gust_of_Wind/Gust_of_Wind.js
@@ -7,7 +7,7 @@ const MACRONAME = "Gust_of_Wind.0.2.js"
 const MACRO = MACRONAME.split(".")[0]     // Trim of the version number and extension
 let trcLvl = 1;
 jez.trc(2, trcLvl, `=== Starting === ${MACRONAME} ===`);
-for (let i = 0; i < args.length; i++) jez.trc(3, trcLvl`  args[${i}]`, args[i]);
+for (let i = 0; i < args.length; i++) jez.trc(3, trcLvl,`  args[${i}]`, args[i]);
 const LAST_ARG = args[args.length - 1];
 let aActor;         // Acting actor, creature that invoked the macro
 if (LAST_ARG.tokenId) aActor = canvas.tokens.get(LAST_ARG.tokenId).actor; 
